01aed6a71bf85761574afd81105f5029cbaf8197
Davi Nepomuceno <davi.nepomuceno@icloud.com>
2025-08-04 10:00:25 -0300

    StreamLit Dashboard

---
+++ 1_🏠_home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("# FIFA 2023 OFFICIAL DATASET!")
+
+st.sidebar.markdown("Desenvolvido por [Davi Nepomuceno](https://github.com/davimp3), créditos ao Prof. Rodrigo Tadewald - Asimov Academy")
+
+
